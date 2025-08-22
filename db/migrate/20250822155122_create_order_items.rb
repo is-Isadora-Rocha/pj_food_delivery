ee42b3e2ea53f163abc9b49ebe5b1455222c8484
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2025-08-22 12:51:47 -0300

    scaffold OrderItem

---
+++ db/migrate/20250822155122_create_order_items.rb
@@ -0,0 +1,14 @@
+class CreateOrderItems < ActiveRecord::Migration[7.2]
+  def change
+    create_table :order_items do |t|
+      t.references :order, null: false, foreign_key: true
+      t.references :menu_item, null: false, foreign_key: true
+      t.integer :quantity
+      t.decimal :unit_price, precision: 10, scale: 2
+      t.decimal :total_price, precision: 10, scale: 2
+      t.string :notes
+
+      t.timestamps
+    end
+  end
+end
